f6822d3188bb8449edebff97aaab349677dfd1ac
Kevin Gao <kevinmingjiang@gmail.com>
2022-02-24 12:33:52 -0500

    get form data

--- src/components/ListForms.jsx
+++ src/components/ListForms.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import axios from "axios";
 import { useEffect } from "react";
 import CircularProgress from "@mui/material/CircularProgress";
@@ -16,11 +16,15 @@ import TextField from "@mui/material/TextField";
 import InputAdornment from "@mui/material/InputAdornment";
 import SearchIcon from "@mui/icons-material/Search";
 import AutoSizer from "react-virtualized-auto-sizer";
+import Modal from "react-modal";
+import Card from "@mui/material/Card";
+import uniqid from "uniqid";
 
 function ListForms() {
 	// Fiyge form data
 	const [formList, setFormList] = useState();
 	const [error, setError] = useState(false);
+	const [mounted, setMounted] = useState(false);
 
 	// Fetch all the list from Fiyge using API call
 	const query = encodeURI(
@@ -32,11 +36,12 @@ function ListForms() {
 
 	useEffect(() => {
 		console.log("API Called");
+		setMounted(true);
 		axios({
 			method: "get",
 			url: `https://builder.fiyge.com/development_base/forms/index.json?q=${query}`,
 			headers: {
-				"Content-Type": "multipart/form-data",
+				// "Content-Type": "multipart/form-data",
 				Authorization: "Bearer " + localStorage.getItem("access"),
 			},
 		})
@@ -50,13 +55,107 @@ function ListForms() {
 			});
 	}, []);
 
-	// Testing
-	console.log(formList);
+	// Search bar
+	const [inputText, setInputText] = useState("");
 
+	let inputHandler = (e) => {
+		var lowerCase = e.target.value.toLowerCase();
+		setInputText(lowerCase);
+	};
+
+	// Modal
+	let subtitle;
+	const [modalIsOpen, setIsOpen] = useState(false);
+	const [formID, setFormID] = useState();
+
+	function openModal(id) {
+		setIsOpen(true);
+		setFormID(id);
+	}
+	function afterOpenModal() {
+		// subtitle.style.color = "#f00";
+	}
+	function closeModal() {
+		setIsOpen(false);
+	}
+
+	const customStyles = {
+		content: {
+			bottom: "auto",
+			width: "900px",
+			height: "85vh",
+			margin: "auto",
+			overflow: "scroll",
+		},
+	};
+
+	// Get all nodes of the double clicked form
+	const [formResult, setFormResult] = useState();
+	const isInitialMount = useRef(true);
+
+	useEffect(() => {
+		console.log("get form nodes called");
+		if (isInitialMount.current) {
+			isInitialMount.current = false;
+		} else {
+			axios({
+				method: "get",
+				url: `https://builder.fiyge.com/development_base/forms/index.json?q={"fields":["forms.*"],"with_recursive":{"forms.id": ${formID}}}`,
+				headers: {
+					"Content-Type": "multipart/form-data",
+					Authorization: "Bearer " + localStorage.getItem("access"),
+				},
+			})
+				.then((res) => {
+					console.log(res);
+					setFormResult(res.data);
+				})
+				.catch((error) => {
+					console.log({ error });
+					setError(true);
+				});
+		}
+	}, [modalIsOpen, formID]);
+
+	// Get all nodes in form
+	function getFormResult(id, formName) {
+		console.log("get form nodes called");
+
+		return (
+			<Modal
+				isOpen={modalIsOpen}
+				onAfterOpen={afterOpenModal}
+				onRequestClose={closeModal}
+				style={customStyles}
+				contentLabel="Example Modal">
+				<div className="modal-header">
+					<h5 className="modal-title">Export JSON</h5>
+					<button
+						type="button"
+						className="btn-close"
+						aria-label="Close"
+						onClick={closeModal}></button>
+				</div>
+				<Card variant="outlined" sx={{ mt: 3, pl: 4, pt: 2 }}>
+					{formResult ? (
+						<div>
+							{`Form: ${formName}`}
+							<br></br>
+							{`ID: ${id}`}
+
+							<pre>{JSON.stringify(formResult, null, 2)}</pre>
+						</div>
+					) : null}
+				</Card>
+			</Modal>
+		);
+	}
+
+	// Render each form name and id in list
 	const renderFormNames = (id, formName) => {
 		return (
 			<ListItem dense key={id} component="div" disablePadding sx={{ pr: 2 }}>
-				<ListItemButton>
+				<ListItemButton onDoubleClick={() => openModal(id)}>
 					{window.innerWidth >= 1680 ? (
 						<ListItemIcon>
 							<FormatAlignLeftIcon />
@@ -67,16 +166,9 @@ function ListForms() {
 						<DeleteIcon />
 					</IconButton>
 				</ListItemButton>
+				{modalIsOpen && formID === id ? getFormResult(id, formName) : null}
 			</ListItem>
 		);
-	};
-
-	// Search bar
-	const [inputText, setInputText] = useState("");
-
-	let inputHandler = (e) => {
-		var lowerCase = e.target.value.toLowerCase();
-		setInputText(lowerCase);
 	};
 
 	// Render the list only after fetching the forms successfully
